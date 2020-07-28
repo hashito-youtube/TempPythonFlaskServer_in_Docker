e94f6d34ada295b595d0c73b3ba5561e9de56fb2
hashito <hasitozzz@gmail.com>
2020-07-28 10:14:32 +0900

    add env

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import os
 app = Flask(__name__)
+
+PORT=os.environ.get("PORT","80")
+
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(port=80,host="0.0.0.0")
+    app.run(port=int(PORT),host="0.0.0.0")
